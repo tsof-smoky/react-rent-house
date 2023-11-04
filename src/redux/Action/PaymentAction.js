4c834bab8bcdd7f25c2c30f4abccdd1d1d646924
lUcgryy <giangttse160068@fpt.edu.vn>
2023-11-05 02:35:38 +0700

    Update PaymentAction.js

--- src/redux/Action/PaymentAction.js
+++ src/redux/Action/PaymentAction.js
@@ -26,7 +26,48 @@ export const payment = (amount, bankCode, input) => async (dispatch) => {
         // for (var pair of formData.entries()) {
         //     console.log(pair[1]);
         // }
-
+        let newTab = window.open();
+        let loader = `<!DOCTYPE html>
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+        .loader {
+          border: 16px solid #f3f3f3;
+          border-radius: 50%;
+          border-top: 16px solid blue;
+          border-right: 16px solid green;
+          border-bottom: 16px solid red;
+          border-left: 16px solid pink;
+          width: 120px;
+          height: 120px;
+          -webkit-animation: spin 2s linear infinite; /* Safari */
+          animation: spin 2s linear infinite;
+          position: fixed;
+          top:-100%; right:-100%; left:-100%; bottom:-100%;
+          margin:auto;
+          z-index:0;
+        }
+        /* Safari */
+        @-webkit-keyframes spin {
+          0% { -webkit-transform: rotate(0deg); }
+          100% { -webkit-transform: rotate(360deg); }
+        }
+        
+        @keyframes spin {
+          0% { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+        }
+        </style>
+        </head>
+        <body>
+        
+        <div class="loader"></div>
+        
+        </body>
+        </html>
+        `
+        newTab.document.write(loader);
         const res = await httpFile.post("/payments/create_payment_url", formData);
 
         dispatch({
@@ -34,7 +75,6 @@ export const payment = (amount, bankCode, input) => async (dispatch) => {
             payload: res.data,
         });
 
-        let newTab = window.open();
         newTab.location.href = res.data.data;
     } catch (error) {
         dispatch({
